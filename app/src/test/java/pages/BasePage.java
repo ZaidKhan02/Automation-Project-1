cf2186e338780d98e853ae522e880d4cb93990fa
Zaid Khan <59452329+ZaidKhan02@users.noreply.github.com>
2025-12-04 22:54:16 -0500

    made the BaseTest and BasePage

---
+++ app/src/test/java/pages/BasePage.java
@@ -0,0 +1,72 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected final WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        this.actions = new Actions(driver);
+    }
+
+    /*
+     * public WebElement findElement(By locator) {
+     * return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+     * }
+     */
+
+    protected WebElement waitForVisibility(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForClickable(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void click(By locator) {
+        // something like click(loginBtn)
+        waitForClickable(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        // in LoginPage, it will be like type(emailField, "zaidlovemath@gmail.com")
+        WebElement element = waitForVisibility(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisibility(locator).getText();
+    }
+
+    /*
+     * protected boolean isElementDisplayed(By locator) {
+     * try {
+     * return waitForVisible(locator).isDisplayed();
+     * } catch (Exception e) {
+     * return false;
+     * }
+     * }
+     * 
+     * protected void waitForInvisibility(By locator) {
+     * wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+     * }
+     * 
+     * protected void hoverOver(By locator) {
+     * WebElement element = waitForVisible(locator);
+     * actions.moveToElement(element).perform();
+     * }
+     */
+
+}
---
+++ app/src/test/java/tests/BaseTest.java
@@ -0,0 +1,64 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected String url;
+
+    @BeforeMethod
+    @Parameters({ "BaseURL" })
+    public void launchBrowser(@Optional("https://automationexercise.com/") String baseURL) {
+        this.url = baseURL;
+        driver = pickBrowser(System.getProperty("browser", "chrome"));
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
+    public WebDriver pickBrowser(String browser) {
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--remote-allow-origins=*");
+                firefoxOptions.addArguments("--disable-notifications");
+                return new FirefoxDriver(firefoxOptions);
+
+            case "edge":
+            case "microsoftedge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                edgeOptions.addArguments("--disable-notifications");
+                return new EdgeDriver(edgeOptions);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--disable-notifications");
+                return new ChromeDriver(chromeOptions);
+        }
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+// add all the stuff for parallel testing, selenium grid, parameters
